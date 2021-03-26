d6682ba09bb34d6e398706454485f84223016e1a
imhilla <hillaryodhiambo@gmail.com>
2021-03-26 14:29:47 +0300

    new update

---
+++ config/application.rb
@@ -0,0 +1,18 @@
+require_relative 'boot'
+
+require 'rails/all'
+require 'appmap/railtie' if defined?(AppMap)
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module SampleApp
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.0
+
+    # Include the authenticity token in remote forms.
+    config.action_view.embed_authenticity_token_in_remote_forms = true
+  end
+end
